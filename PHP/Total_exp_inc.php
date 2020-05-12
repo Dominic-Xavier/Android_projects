8e684fecbb46ff35dad7b13f87a01a836b3e18ec
Dominic-Xavier <63072790+Dominic-Xavier@users.noreply.github.com>
2020-05-12 20:47:28 +0530

    Add files via upload

--- PHP/Total_exp_inc.php
+++ PHP/Total_exp_inc.php
@@ -1,23 +1,23 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=="POST"){
 	include('conn.php');
-	global $connection;
+	global $connection;//This valeable has database connection details
 	expense_income($connection);
 }
 
 function expense_income($connection){
-	$month1=$month2=$month3=$month4=$month5=$month6=$month7=$month8=$month9=$month10=$month4=$month11=$month12='a';
-	$jan_exp=$jan_inc=$feb_exp=$feb_inc=$feb_exp=$feb_inc=$mar_exp=$mar_inc=$apr_exp=$apr_inc=$may_exp=$may_inc=$jun_exp=$jun_inc=$jul_exp=$jul_inc=$aug_exp=$$aug_inc=$aug_inc=$sep_exp=$sep_inc=$oct_exp=$oct_inc=$nov_exp=$nov_inc=$dec_exp=$dec_inc=0;
+	$month1=$month2=$month3=$month4=$month5=$month6=$month7=$month8=$month9=$month10=$month4=$month11=$month12=$month_p="";
+	$exp1=$inc1=$exp2=$inc2=$exp3=$inc3=$exp4=$inc4=$exp5=$inc5=$exp6=$inc6=$exp7=$inc7=$exp8=$inc8=$exp9=$inc9=$exp10=$inc10=$exp11=$inc11=$exp12=$inc12=$exp_p=$inc_p=0;
 	
-	if(!empty($_POST['user_id'])){
-		$user_id = $_POST['user_id'];
+	if(!empty($_POST['User_id'])&&!empty($_POST['sdate'])&&!empty($_POST['edate'])){
+		$user_id = $_POST['User_id'];
 		$u_id_income = $user_id."income";
 		$expense = array();
 		$u_id_expense = $user_id."expense";
 		$income = array();
 		$Inc_Exp_arr = array();
-		$query = "SELECT YEAR(date) AS 'year', MONTH(date) AS 'month', Exp_Amt FROM $u_id_expense";
-		$sql = "SELECT YEAR(date) AS 'year', MONTH(date) AS 'month', Inc_Amt FROM $u_id_income";
+		$query = "SELECT YEAR(date) AS 'year', MONTH(date) AS 'month', Exp_Amt FROM $u_id_expense";//Expense table
+		$sql = "SELECT YEAR(date) AS 'year', MONTH(date) AS 'month', Inc_Amt FROM $u_id_income";//Income table
 		$result_exp = mysqli_query($connection,$query);
 		$result_inc = mysqli_query($connection,$sql);
 		if($result_exp && $result_inc){
@@ -25,77 +25,77 @@ function expense_income($connection){
 				$d = $row['month'];
 				$exp_amt = $row['Exp_Amt'];
 				$inc_amt = $row1['Inc_Amt'];
-					switch($d){
+					switch($d){ //trying to figure out month wise details using month
 					case 1:{
 						$month1 = "Jan";
-						$jan_exp = $jan_exp+$exp_amt;
-						$jan_inc = $jan_inc+$inc_amt;
+						$exp1 = $exp1+$exp_amt;
+						$inc1 = $inc1+$inc_amt;
 						break;
 					}
 					case 2:{
 						$month2 = "Feb";
-						$feb_exp = $feb_exp+$exp_amt;
-						$feb_inc = $feb_inc+$inc_amt;
+						$exp2 = $exp2+$exp_amt;
+						$inc2 = $inc2+$inc_amt;
 						break;
 					}
 					case 3:{
 						$month3 = "Mar";
-						$mar_exp = $mar_exp+$exp_amt;
-						$mar_inc = $mar_inc+$inc_amt;
+						$exp3 = $exp3+$exp_amt;
+						$inc3 = $inc3+$inc_amt;
 						break;
 					}
 					case 4:{
 						$month4 = "Apr";
-						$apr_exp = $apr_exp+$exp_amt;
-						$apr_inc = $apr_inc+$inc_amt;
+						$exp4 = $exp4+$exp_amt;
+						$inc4 = $inc4+$inc_amt;
 						break;
 					}
 					case 5:{
 						$month5 = "May";
-						$may_exp = $may_exp+$exp_amt;
-						$may_inc = $may_inc+$inc_amt;
+						$exp5 = $exp5+$exp_amt;
+						$inc5 = $inc5+$inc_amt;
 						break;
 					}
 					case 6:{
 						$month6 = "June";
-						$jun_exp = $jun_exp+$exp_amt;
-						$jun_inc = $jun_inc+$inc_amt;
+						$exp6 = $exp6+$exp_amt;
+						$inc6 = $inc6+$inc_amt;
 						break;
 					}
 					case 7:{
 						$month7 = "July";
-						$jul_exp = $jul_exp+$exp_amt;
-						$jul_inc = $jul_inc+$inc_amt;
+						$exp7 = $exp7+$exp_amt;
+						$inc7 = $inc7+$inc_amt;
 						break;
 					}
 					case 8:{
 						$month8 = "Aug";
-						$aug_exp = $aug_exp+$exp_amt;
-						$aug_inc = $aug_inc+$inc_amt;
+						$exp8 = $exp8+$exp_amt;
+						$inc8 = $inc8+$inc_amt;
 						break;
 					}
 					case 9:{
 						$month9 = "Sep";
-						$sep_exp = $sep_exp+$exp_amt;
-						$sep_inc = $sep_inc+$inc_amt;
+						$exp9 = $exp9+$exp_amt;
+						$inc9 = $inc9+$inc_amt;
 						break;
 					}
 					case 10:{
 						$month10 = "Oct";
-						$oct_exp = $oct_exp+$exp_amt;
-						$oct_inc = $oct_inc+$inc_amt;
+						$exp10 = $exp10+$exp_amt;
+						$inc10 = $inc10+$inc_amt;
 						break;
 					}
 					case 11:{
 						$month11 = "Nov";
-						$nov_exp = $nov_exp+$exp_amt;
-						$nov_inc = $nov_inc+$inc_amt;
+						$exp11 = $exp11+$exp_amt;
+						$inc11 = $inc11+$inc_amt;
 						break;
 					}
 					case 12:{
 						$month12 = "Dec";
-						$dec_exp = $dec_exp+$exp_amt;
-						$dec_inc = $dec_inc+$inc_amt;
+						$exp12 = $exp12+$exp_amt;
+						$inc12 = $inc12+$inc_amt;
 						break;
 					}
 				}
@@ -104,37 +104,34 @@ function expense_income($connection){
 		}
 			$Inc_Exp_arr = array_merge($expense,$income);
 			$merge_json = json_encode($Inc_Exp_arr);
-			echo $merge_json;
+			//echo $merge_json;
 			
-			$datas = array();
+			//Output of $merge_json
+			//$merge_json = [{"year":"2020","month":"5","Exp_Amt":"23"},{"year":"2020","month":"5","Exp_Amt":"60"},{"year":"0","month":"0","Exp_Amt":"23"},{"year":"0","month":"0","Exp_Amt":"56"},{"year":"2020","month":"5","Exp_Amt":"23"},{"year":"2020","month":"5","Exp_Amt":"56"},{"year":"2020","month":"5","Exp_Amt":"23"},{"year":"2020","month":"5","Exp_Amt":"56"},{"year":"2020","month":"5","Inc_Amt":"23"},{"year":"2020","month":"5","Inc_Amt":"60"},{"year":"2020","month":"5","Inc_Amt":"23"},{"year":"2020","month":"5","Inc_Amt":"60"},{"year":"2020","month":"5","Inc_Amt":"23"},{"year":"2020","month":"5","Inc_Amt":"23"},{"year":"2020","month":"5","Inc_Amt":"56"},{"year":"2020","month":"5","Inc_Amt":"23"}]
 			
-			$obj = json_decode($merge_json,true);
-			foreach($obj as $key => $value){
-				$month = $value['month'];
-				$amt = $value['Inc_Amt'];
-				$datas = array('month'=>$month,'Amount'=>$amt);
-			}
-			print_r($datas);
 			
-			/* for($i=1;$i<=12;$i++){
-				
-			}
-			(
-			'month'=>$month1,'Expense'=>$jan_exp,'income'=>$jan_inc,
-			'month'=>$month2,'Expense'=>$feb_exp,'income'=>$feb_inc,
-			'month'=>$month3,'Expense'=>$mar_exp,'income'=>$mar_inc,
-			'month'=>$month4,'Expense'=>$apr_exp,'income'=>$apr_inc,
-			'month'=>$month5,'Expense'=>$may_exp,'income'=>$may_inc,
-			'month'=>$month6,'Expense'=>$jun_exp,'income'=>$jun_inc,
-			'month'=>$month7,'Expense'=>$jul_exp,'income'=>$jul_inc,
-			'month'=>$month8,'Expense'=>$aug_exp,'income'=>$aug_inc,
-			'month'=>$month9,'Expense'=>$sep_exp,'income'=>$sep_inc,
-			'month'=>$month10,'Expense'=>$oct_exp,'income'=>$oct_inc,
-			'month'=>$month11,'Expense'=>$nov_exp,'income'=>$nov_inc,
-			'month'=>$month12,'Expense'=>$dec_exp,'income'=>$dec_inc
-			);
-			$all_data = json_encode($datas);
+			$datas = array(
+			array('month'=>$month1,'Expense'=>$exp1,'income'=>$inc1,'total'=>$inc1-$exp1),
+			array('month'=>$month2,'Expense'=>$exp2,'income'=>$inc2,'total'=>$inc2-$exp2),
+			array('month'=>$month3,'Expense'=>$exp3,'income'=>$inc3,'total'=>$inc3-$exp3),
+			array('month'=>$month4,'Expense'=>$exp4,'income'=>$inc4,'total'=>$inc4-$exp4),
+			array('month'=>$month5,'Expense'=>$exp5,'income'=>$inc5,'total'=>$inc5-$exp5),
+			array('month'=>$month6,'Expense'=>$exp6,'income'=>$inc6,'total'=>$inc6-$exp6),
+			array('month'=>$month7,'Expense'=>$exp7,'income'=>$inc7,'total'=>$inc7-$exp7),
+			array('month'=>$month8,'Expense'=>$exp8,'income'=>$inc8,'total'=>$inc8-$exp8),
+			array('month'=>$month9,'Expense'=>$exp9,'income'=>$inc9,'total'=>$inc9-$exp9),
+			array('month'=>$month10,'Expense'=>$exp10,'income'=>$inc10,'total'=>$inc10-$exp10),
+			array('month'=>$month11,'Expense'=>$exp11,'income'=>$inc11,'total'=>$inc11-$exp11),
+			array('month'=>$month12,'Expense'=>$exp12,'income'=>$inc12,'total'=>$inc12-$exp12));
+			
+		$Total_Value = json_encode($datas);
+			
+			echo $Total_Value;
+			
+			/* $all_data = json_encode($datas);
 			echo $all_data; */
+			//This is the o/t iam getting. Currently I have only may month data in my database.
+			//echo $all_data = {"month":"","Expense":0,"income":0}
 		}
 	}
 }
