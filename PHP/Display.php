686958fc28e7669d01ceeb10a31c78097b36a4d8
Dominic-Xavier <63072790+Dominic-Xavier@users.noreply.github.com>
2020-06-13 17:43:11 +0530

    Add files via upload

--- PHP/Display.php
+++ PHP/Display.php
@@ -4,16 +4,18 @@
 		require 'conn.php';
 		$st_date = $_POST['sdate'];
 		$e_date = $_POST['edate'];
-		if(!empty($_POST['User_id']) && !empty($st_date) && !empty($e_date))
-			display($connection,$_POST['User_id'],$_POST['sdate'],$_POST['edate']);
+		$keyword = $_POST['keyword'];
+		if(!empty($_POST['User_id']) && !empty($st_date) && !empty($e_date) && !empty($keyword))
+			display($connection,$_POST['User_id'],$_POST['sdate'],$_POST['edate'],$_POST['keyword']);
 		else
-			Total_Expense_Income_MonthWise($connection,$_POST['User_id'],$st_date);
+			//Total_Expense_Income_MonthWise($connection,$_POST['User_id'],$st_date);
+		echo "Error occured";
 	}
 	else{
 		echo "Please give post request";
 	}
 	
-	function display($connection,$User_id,$start_date,$en_date){
+	function display($connection,$User_id,$start_date,$en_date,$keyword){
 			$start = $start_date;
 			$end = $en_date;
 			$table_name = $User_id;
@@ -21,12 +23,16 @@
 			$en = date_create($end);
 			$st_date = date_format($st,"Y-m-d");
 			$end_date = date_format($en,"Y-m-d");
-			$query = "select Date,Exp_Des,Exp_Amt from $table_name where Date between '$st_date' and '$end_date'";
-			$result = mysqli_query($connection,$query);
+			$query1 = query($keyword,$table_name,$st_date,$end_date);
+			$result = mysqli_query($connection,$query1);
 			$datas = array();
 			if($result){
+				$des = "";
 				while($row = mysqli_fetch_assoc($result)){
-					$des = $row['Exp_Des'];
+					if($keyword=="expense")
+						$des = $row['Exp_Des'];
+					else
+						$des = $row['Inc_Des'];
 					//$amt = $row['Exp_Amt'];
 					if($des!="-"){
 						$datas[] = $row;
@@ -52,9 +58,9 @@
 			$datas = array();
 			if($result){
 				while($row = mysqli_fetch_assoc($result)){
-					$des = $row['Exp_Des'];
-					$amt = $row['Exp_Amt'];
-					if($des!="-" ){
+					$Exp_Des = $row['Exp_Des'];
+					$Inc_Des = $row['Inc_Des'];
+					if($Exp_Des!="-" && $Inc_Des!="-"){
 						$datas[] = $row;
 					}
 				}
@@ -65,5 +71,14 @@
 				echo"Error occured".mysqli_error($connection);
 			}
 		mysqli_close($connection);
+	}
+	
+	function query($Keyword,$table_name,$start_date,$end_date){
+		if($Keyword == "expense")
+			$sql = "select Date,Exp_Des,Exp_Amt from $table_name where Date between '$start_date' and '$end_date'";
+		else if($Keyword == "income")
+			$sql = "select Date,Inc_Des,Inc_Amt from $table_name where Date between '$start_date' and '$end_date'";
+		
+		return $sql;
 	}
 ?>
